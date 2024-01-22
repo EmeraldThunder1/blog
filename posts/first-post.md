707b0983f156d2f2bfaed9f44d8d40271f92fa4b
EmeraldThunder1 <git_hub@dapoole.co.uk>
2024-01-22 14:23:04 +0000

    Initial commit

---
+++ posts/first-post.md
@@ -0,0 +1,2 @@
+# Hello!
+This is a test post for my github pages blog. This is some new content
---
+++ scripts/main.py
@@ -0,0 +1,39 @@
+# To be run as an action when the repository is pushed to
+# Load all the posts and use them to fill in the templates
+
+import os
+from os.path import join, dirname
+import yaml
+from datetime import datetime
+import markdown
+from bs4 import BeautifulSoup
+from jinja2 import Template
+
+def main():
+    with open("meta.yml", "r") as f:
+        meta = yaml.load(f, Loader=yaml.FullLoader)
+    
+    posts = os.listdir("./posts")
+
+    with open(join(dirname(__file__), "../templates/post.jinja"), "r") as f:
+        postTemplate = Template(f.read())
+
+    for post in posts:
+        if post.endswith('.md'):
+            with open(f"./posts/{post}", 'r') as f:
+                creationDate = datetime.fromtimestamp(os.path.getctime(f"./posts/{post}")).strftime("%m/%d/%Y")
+                modDate = datetime.fromtimestamp(os.path.getmtime(f"./posts/{post}")).strftime("%m/%d/%Y")
+
+                content = markdown.markdown(f.read())
+                soup = BeautifulSoup(content, 'html.parser')
+
+                title = soup.find('h1').text
+
+                renderedPost = postTemplate.render(postTitle=title, blogTitle=meta["title"], description=meta["description"], postContent=content)
+                
+                if not os.path.exists(post.split('.')[0]):
+                    os.makedirs(post.split(".")[0])
+                with open(f"{post.split('.')[0]}/index.html", "w+") as f:
+                    f.write(renderedPost)
+
+main()
